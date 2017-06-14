866a6ab50adb5d62127b58983a91cc67107bdd7c
James Messinger <james.messinger@gmail.com>
2017-06-14 07:08:29 -0500

    uncommented the test

--- test/specs/external-refs/parse.spec.js
+++ test/specs/external-refs/parse.spec.js
@@ -4,19 +4,18 @@
   describe('Parsing a schema with external $refs', function () {
 
     it('should only parse the root file', function () {
-      return Promise.resolve();
-      // return jsonSchemaLib.parse(path.abs('specs/external-refs/schema/vehicle.json'))
-      //   .then(function (schema) {
-      //     assert.validSchema(schema);
+      return jsonSchemaLib.parse(path.abs('specs/external-refs/schema/vehicle.json'))
+        .then(function (schema) {
+          assert.validSchema(schema);
 
-      //     // The schema should only contain the root file,
-      //     // and the files that are directly referenced by the root file
-      //     assert.validFiles(schema.files, [
-      //       path.abs('specs/external-refs/schema/vehicle.json'),
-      //       path.abs('specs/external-refs/schema/body.json'),
-      //       path.abs('specs/external-refs/schema/drivetrain.json'),
-      //     ]);
-      //   });
+          // The schema should only contain the root file,
+          // and the files that are directly referenced by the root file
+          assert.validFiles(schema.files, [
+            path.abs('specs/external-refs/schema/vehicle.json'),
+            path.abs('specs/external-refs/schema/body.json'),
+            path.abs('specs/external-refs/schema/drivetrain.json'),
+          ]);
+        });
     });
 
   });
