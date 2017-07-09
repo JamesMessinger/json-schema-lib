202655d381762bc4b48e31552bfc8bacf272c4ba
James Messinger <james.messinger@gmail.com>
2017-07-09 15:41:51 -0500

    added a parameter to return plugins sorted vs. unsorted

--- test/fixtures/helpers.js
+++ test/fixtures/helpers.js
@@ -22,25 +22,52 @@
     /**
      * Returns an array of POJOs that matches the default JsonSchemaLib plugins
      *
+     * @param {boolean} sorted
      * @returns {object[]}
      */
-    getDefaultPlugins: function getDefaultPlugins () {
-      return host.node
-        ? [
-          { name: 'NodeUrlPlugin', priority: 20 },
-          { name: 'FileSystemPlugin', priority: 10 },
-          { name: 'HttpPlugin', priority: 5 },
-          { name: 'BufferDecoderPlugin', priority: 10 },
-          { name: 'ArrayDecoderPlugin', priority: 5 },
-          { name: 'JsonPlugin', priority: 20 },
-        ]
-        : [
-          { name: 'BrowserUrlPlugin', priority: 20 },
-          { name: 'XMLHttpRequestPlugin', priority: 5 },
-          { name: 'TextDecoderPlugin', priority: 10 },
-          { name: 'ArrayDecoderPlugin', priority: 5 },
-          { name: 'JsonPlugin', priority: 20 },
-        ];
+    getDefaultPlugins: function getDefaultPlugins (sorted) {
+      if (host.node) {
+        if (sorted) {
+          return [
+            { name: 'NodeUrlPlugin', priority: 20 },
+            { name: 'JsonPlugin', priority: 20 },
+            { name: 'FileSystemPlugin', priority: 10 },
+            { name: 'BufferDecoderPlugin', priority: 10 },
+            { name: 'HttpPlugin', priority: 5 },
+            { name: 'ArrayDecoderPlugin', priority: 5 },
+          ];
+        }
+        else {
+          return [
+            { name: 'NodeUrlPlugin', priority: 20 },
+            { name: 'FileSystemPlugin', priority: 10 },
+            { name: 'HttpPlugin', priority: 5 },
+            { name: 'BufferDecoderPlugin', priority: 10 },
+            { name: 'ArrayDecoderPlugin', priority: 5 },
+            { name: 'JsonPlugin', priority: 20 },
+          ];
+        }
+      }
+      else {
+        if (sorted) {
+          return [
+            { name: 'BrowserUrlPlugin', priority: 20 },
+            { name: 'JsonPlugin', priority: 20 },
+            { name: 'TextDecoderPlugin', priority: 10 },
+            { name: 'XMLHttpRequestPlugin', priority: 5 },
+            { name: 'ArrayDecoderPlugin', priority: 5 },
+          ];
+        }
+        else {
+          return [
+            { name: 'BrowserUrlPlugin', priority: 20 },
+            { name: 'XMLHttpRequestPlugin', priority: 5 },
+            { name: 'TextDecoderPlugin', priority: 10 },
+            { name: 'ArrayDecoderPlugin', priority: 5 },
+            { name: 'JsonPlugin', priority: 20 },
+          ];
+        }
+      }
     },
   };
 
